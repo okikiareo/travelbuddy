36125bff00f996c97038d4d2cda76deb52f27cfe
Okiki Areo <okikiareo@gmail.com>
2023-11-14 12:05:45 +0100

    signup commit

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,33 +5,34 @@ import { Form, Link } from "react-router-dom";
 
 const Signup = ()=> {
     return(
-<main className="signup">
-<section className="signup_wrapper wrap flex">
-                <Link className="signup_home flex" to="/">
-                    <img className="signup_back" src={backIcon} alt="" />
+<main className="signup main">
+<section className="main_wrapper wrap flex">
+                <Link className="back_home flex" to="/">
+                    <img className="back_img" src={backIcon} alt="" />
                 </Link>
-                <Form className="signup__form">
-                    <h1 className="signup__form-title">
-                        Login
+                <Form className="all_form">
+                    <h1 className="form-title">
+                        Sign up
                     </h1>
-                    <div className="signup__form-field flex">
-                       
-                        <input className="signup__form-input" type="email" name="email" id="" />
+                    <p>Please fill the details and create an account</p>
+                    <div className="form-field flex">
+                        <input className="form-input" type="text" name="name" id="" placeholder="Full Name" />
                     </div>
-                    <div className="login__form-inner">
-                        <div className="signup__form-field flex">
-                           
-                            <input className="signup__form-input" type="email" name="email" id="" />
-                            <img className="signup__form-pword" src={showIcon} alt="" />
+                    <div className="form-field flex">
+                        <input className="form-input" type="email" name="email" id="" placeholder="School Email" />
+                    </div>
+                    <div className="form-field flex">
+                            <input className="form-input" type="password" name="password" id=""  placeholder="Password"/>
+                            <img className="form-pword" src={showIcon} alt="" />
                         </div>
-                        <Link className="signup__form-aside" to="">
-                            Forgot password
-                        </Link>
-                    </div>
-                    <Link className="signup__form-login" to="">Login</Link>
-                    <div className="signup__form-logup">
-                        New user?
-                        <Link className="signup__form-signu" to="">Sign up</Link>
+                        <div className="form-field flex">
+                            <input className="form-input" type="password" name="password" id=""  placeholder="Confirm Password"/>
+                            <img className="form-pword" src={showIcon} alt="" />
+                        </div>
+                    <Link className="login__form-login continue" to="">Continue</Link>
+                    <div className="form-logup">
+                        Already have an account?
+                        <Link className="form-signu" to="">Sign in</Link>
                     </div>
                 </Form>
              
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,10 +3,10 @@ import buddy from "../assets/images/buddy.png";
 import backIcon from "../assets/images/icon-left.svg";
 const Welcome =() =>{
     return(
-<main className="welcome">
-<section className="welcome_wrapper wrap flex">
-                <Link className="welcome_home flex" to="/">
-                    <img className="login_back" src={backIcon} alt="" />
+<main className="welcome main">
+<section className="main_wrapper wrap flex">
+                <Link className="back_home flex" to="/">
+                    <img className="back_img" src={backIcon} alt="" />
                 </Link>
                 <div className="welcome_info flex">
                     <img src={buddy} alt=""/>
